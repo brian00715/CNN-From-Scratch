1d5af420aaf0dc914d6796290f5b8c79350dc130
SimonKenneth <smkk00715@gmail.com>
2023-11-19 12:48:35 +0800

    refactor; refined data loading and split

--- src/test.m
+++ src/test.m
@@ -1,18 +1,29 @@
 % function lr= lrSchedule(lr_min,lr_max,curr_epoch,total_epoch,options)
 % test lrSchedule
 
-total_epoch = 50;
-lr = [];
-options.method = "cosine";
-options.lr_min = 1e-4;
-options.lr_max = 0.1;
+% total_epoch = 30;
+% lr = [];
+% options.lr_max = 0.01;
+% options.lr = options.lr_max;
+% options.lr_min = 1e-8;
+% options.lr_method = 'cosine_cyclic';
+% options.lr_duty = 5; % duty cycle for cosine lr
+% 
+% for curr_epoch = 1:total_epoch
+%     curr_lr = lrSchedule(curr_epoch, total_epoch, options);
+%     lr = [lr curr_lr];
+% end
+% 
+% plot(lr);
+% xlabel('epoch');
+% ylabel('learning rate');
+% title('lrSchedule test');
 
-for curr_epoch = 1:total_epoch
-    curr_lr = lrSchedule(curr_epoch, total_epoch, options);
-    lr = [lr curr_lr];
-end
+data_path = "../data/p_dataset_26/";
+dataset_option.shuffle = true;
+dataset_option.img_dim = 124;
+dataset_option.train_ratio = 0.8;
+dataset_option.save = true;
+dataset_option.transform = true;
+[images_train, labels_train, images_test, labels_test] = loadDataset(data_path, dataset_option);
 
-plot(lr);
-xlabel('epoch');
-ylabel('learning rate');
-title('lrSchedule test');
